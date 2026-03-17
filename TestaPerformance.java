deec04bda81056bf86a2666a230e84cb52922901
AnaWulCard <156938112+anapaulaw2024@users.noreply.github.com>
2026-03-17 19:43:19 -0300

    Add files via upload
    Código da atividade

---
+++ TestaPerformance.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestaPerformance{
+    public static void main(String[] args) {
+        System.out.println("Inicializando...");
+        long inicio = System.currentTimeMillis();
+        //Collection <Integer> teste = new ArrayList <>();
+        Collection<Integer> teste =new HashSet<>();
+
+        int total = 200000;
+        for (int i = 0; i < total; i++){
+            teste.add(i);
+        }
+        for (int i = 0; i < total; i++){
+            teste.contains(i);
+        }
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        System.out.println("Tempo gasto: " + tempo);
+    }
+}
